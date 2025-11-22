4262526437f6e20d4e51c2482b6339580d9b4e4e
lethank <lethanek93@gmail.com>
2025-11-22 16:46:34 -0500

    added new tool

---
+++ src/tools/create-cms-from-cmp.ts
@@ -0,0 +1,27 @@
+import { tool, ParameterType } from "@optimizely-opal/opal-tools-sdk";
+
+interface ContentParameters {
+  title: string;
+}
+
+async function createContent(parameters: ContentParameters) {
+  const { title } = parameters;
+
+  return {
+    title
+  };
+}
+
+tool({
+  name: "create_cms_from_cmp",
+  description:
+    "Gets content from a CMP and creates a CMS entry",
+  parameters: [
+    {
+      name: "title",
+      type: ParameterType.String,
+      description: "title for the content",
+      required: true,
+    }
+  ],
+})(createContent);
